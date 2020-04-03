be26ba10f04cab4d7e00754cabc5b3117ec664d9
Daniel Andrew <dwandrew@hotmail.com>
2020-04-03 15:12:12 +0000

    Done.

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,10 @@
 def find_min_value(array)
-  # Add your solution here
+  min = array[0]
+  array.length.times{
+    |index|
+    if array[index]<min
+      min = array[index]
+    end
+  }
+  min
 end
